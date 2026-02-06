23215f16cc7dc3d4652004fc7acdac8b7c4b10dd
Kosmatheo <Kosmafzar@gmail.com>
2026-02-06 08:31:12 +0100

    DzialaWys

--- app/src/main/java/com/example/listazakupowrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/listazakupowrecyclerview/MainActivity.java
@@ -7,8 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ZakupyAdapter zakupyAdapter;
+    private ArrayList<Produkt> listaZakopowArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,17 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+        recyclerView = findViewById(R.id.recyc);
+        listaZakopowArrayList.add(new Produkt("mąka"));
+        listaZakopowArrayList.add(new Produkt("mleko"));
+        listaZakopowArrayList.add(new Produkt("cukier"));
+        listaZakopowArrayList.add(new Produkt("cynamon"));
+        listaZakopowArrayList.add(new Produkt("drożdże"));
+
+        zakupyAdapter = new ZakupyAdapter(MainActivity.this , listaZakopowArrayList);
+        recyclerView.setAdapter(zakupyAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
     }
 }
